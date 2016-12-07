2384702475db83f9c15bdd7d80a4a044561792fc
Casper Beyer <caspervonb@icloud.com>
2016-12-07 14:14:19 +0800

    Alter the example

--- demo/script.js
+++ demo/script.js
@@ -1,5 +1,3 @@
 var token = Math.random();
 
-setInterval(function() {
-  document.body.innerHTML = 'Hello World, the token is ' + token;
-}, 1000);
+document.querySelector('span').innerHTML = token;
